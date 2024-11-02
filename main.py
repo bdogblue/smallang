b6a1054ff74192bb7dcb0f317d392b9f37d04d71
bdogblue <bdogblue@gmail.com>
2024-11-02 17:00:43 -0400

    added agree con fuction

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ lexicon = {
 # AGREE = Assign a violation mark for every cluster of segments that does not agree in voicing. 
 # IDENTVOICE = Assign a violation mark for every output that does not agree in voicing with its corresponding input.
 Con = {
-    AGREE
-    IDENTVOICE
+    'AGREE'
+    'IDENTVOICE'
 }
 #Cand is the set of all candidates for a given input.
 Cand_dict = {
@@ -28,6 +28,27 @@ Cand_dict = {
     '/kat + s/' :{'kats','katz'},
 }
 
+def get_feature(input_segment, input_feature):
+    for seg in features_dict:
+        if seg == input_segment:
+            return features_dict.get(seg).get(input_feature)
+    print("ERROR: segment '" + input_segment + "' is undefined" )
+    return "e"
+
 
 def agree(input_candidate):
-    for n in range(len(input_candidate) - 1): 
+    for n in range(len(input_candidate) - 1):
+        current_segment = input_candidate[n]
+        next_segment = input_candidate[n + 1]
+
+        current_feat = get_feature(current_segment, "voice")
+        next_feat = get_feature(next_segment, "voice")
+
+        if current_feat != next_feat:
+            return True
+        
+    return False
+
+print(agree("dogs"))
+print(agree("dogz"))
+print(agree("dogb"))
